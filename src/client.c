aecac486ced6539cd7921364b676a32bc6bb15d2
Cristiano Fernandes <cristianobfernandes@gmail.com>
2010-05-29 04:17:07 -0300

    Utilizando getopt para receber a porta do cliente por parâmetro.
    Signed-off-by: Cristiano Fernandes <cristianobfernandes@gmail.com>

--- src/client.c
+++ src/client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -13,10 +14,32 @@
 #include <connection.h>
 #include <cmd_parser.h>
 
-int main()
+int main (int argc, char **argv)
 {
 	char *cmd, *hist;
 	char prompt[] = "client> ";
+	int opt, opts = 0;
+
+
+	opterr = 0;
+	client_port = 0;
+
+	while ((opt = getopt(argc, argv, "s:")) != -1) {
+		opts++;
+		switch (opt) {
+			case 's':
+				client_port = atoi(optarg);
+				break;
+			case '?':
+			default:
+				printf("Usage: %s -s <port to listen>\n", argv[0]);
+				exit(0);
+		}
+	}
+	if (!opts) {
+		printf("Usage: %s -s <port to listen>\n", argv[0]);
+		exit(0);
+	}
 
 	/* A ordem da tabela afeta o roteamento, a prioridade é sempre da regra mais
 	 * antiga. */
