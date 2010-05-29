49ce07ed2161fa5cd4466ef0a7821267dfdc9d0c
Cristiano Fernandes <cristianobfernandes@gmail.com>
2010-05-29 01:46:23 -0300

    Adicionando estrutura de data para ser enviada no pacote. Adicionadas funções para leitura de dados do file e criação da estrutura de data.
    Signed-off-by: Cristiano Fernandes <cristianobfernandes@gmail.com>

---
+++ include/data.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct data_info
+{
+	char *name;
+	size_t size;
+	void *data;
+};
+
+long int file_size(FILE *fp);
+struct data_info *load_data(char *file_path);
+void dump_data(struct data_info *dinfo);
---
+++ src/data.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <data.h>
+
+long int file_size(FILE *fp)
+{
+	long size;
+
+	fseek(fp , 0 , SEEK_END);
+	size = ftell(fp);
+	rewind(fp);
+
+	return size;
+}
+
+struct data_info *load_data(char *file_path)
+{
+	FILE *fp;
+	long len;
+	struct data_info *dinfo;
+
+	if (!(fp = fopen(file_path, "rb"))) {
+		printf("Error opening file.\n");
+		return NULL;
+	}
+	len = file_size(fp);
+	dinfo = malloc(sizeof(struct data_info));
+	/* TODO: Parsear o nome */
+	dinfo->name = strdup(file_path);
+	dinfo->data = malloc(len);
+	fread(dinfo->data, len, 1, fp);
+	fclose(fp);
+
+	return dinfo;
+}
+
+void dump_data(struct data_info *dinfo)
+{
+	printf("Dump data info\n");
+	printf("Name: %s\n", dinfo->name);
+	printf("Size: %d\n\n", dinfo->size);
+}
